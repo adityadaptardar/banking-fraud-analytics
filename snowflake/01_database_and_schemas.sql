5696687d1481e9f9bb70fd27d472e6f3dbc95889
Aditya Daptardar <adityadaptardar@gmail.com>
2025-12-25 18:21:24 +0530

    Create database and schemas for bank fraud analytics

---
+++ snowflake/01_database_and_schemas.sql
@@ -0,0 +1,11 @@
+create or replace database BANK_FRAUD_ANALYTICS;
+--show databases;
+
+use BANK_FRAUD_ANALYTICS;
+
+create or replace schema RAW;
+create or replace schema STAGING;
+create or replace schema MART;
+create or replace schema SECURE;
+
+--show schemas;
